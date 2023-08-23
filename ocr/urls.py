13dcced5e81a2e247d09c20177a8b0f2cb25e884
pragyananda <82259244+pragyananda@users.noreply.github.com>
2023-08-24 02:46:23 +0530

    add

--- ocr/urls.py
+++ ocr/urls.py
@@ -20,9 +20,11 @@ from action import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('action.urls'),name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
