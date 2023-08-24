719cec708bbb2a149f8167871690d1166f8081f6
pragyananda <82259244+pragyananda@users.noreply.github.com>
2023-08-24 12:34:36 +0530

    add

--- action/urls.py
+++ action/urls.py
@@ -26,5 +26,3 @@ urlpatterns = [
     path('update_cell/', views.update_cell, name='update_cell'),
     path('archive/', views.archive, name='archive'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
